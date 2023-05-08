19ceda90d86bc1631e94bb6abb7da81dbf472d62
Milton Lash <iblash@live.com>
2023-05-08 01:14:09 +0000

    Controllers updates

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -2,6 +2,7 @@ class UserAuthenticationController < ApplicationController
   # Uncomment line 3 in this file and line 5 in ApplicationController if you want to force users to sign in before any other actions.
   # skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie] })
 
+
   def sign_in_form
     render({ :template => "user_authentication/sign_in.html.erb" })
   end
@@ -41,8 +42,8 @@ class UserAuthenticationController < ApplicationController
     @user.email = params.fetch("query_email")
     @user.password = params.fetch("query_password")
     @user.password_confirmation = params.fetch("query_password_confirmation")
-    @user.comments_count = params.fetch("query_comments_count")
-    @user.likes_count = params.fetch("query_likes_count")
+    @user.comments_count = 0
+    @user.likes_count = 0
     @user.private = params.fetch("query_private", false)
     @user.username = params.fetch("query_username")
 
