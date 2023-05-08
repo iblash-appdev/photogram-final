337f86cc100b3e4bc035951ac578a91098bd4ca6
Milton Lash <iblash@live.com>
2023-05-08 00:50:36 +0000

    Add Avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :integer          not null, primary key
+#  avatar          :string
 #  comments_count  :integer
 #  email           :string
 #  likes_count     :integer
